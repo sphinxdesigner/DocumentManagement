64a83339b67ef905646aaf850c4af5240ad26408
sphinxdesigner <sphinxdesigner8@gmail.com>
2025-03-28 11:49:26 +0530

    s
    Initial

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+document
+  .getElementById("click-upload")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    document.getElementById("file-upload").click(); 
+  });
+
+  
