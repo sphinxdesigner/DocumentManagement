4017cb359ddaf6f052a687616a2904645dffc25b
sphinxdesigner <sphinxdesigner8@gmail.com>
2025-04-01 11:51:01 +0530

    s
    s

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ document
   .getElementById("click-upload")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    document.getElementById("file-upload").click(); 
+    document.getElementById("file-upload").click();
   });
 
-  
+
